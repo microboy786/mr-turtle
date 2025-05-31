cf2160187a9b143f199b1d1be0b0c0aad953345e
microboy786 <kavithasupramanium@gmail.com>
2025-05-30 23:36:48 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    turtle.forward(1)
-})
-input.onGesture(Gesture.ScreenUp, function () {
-    turtle.back(1)
+    turtle.turnLeft()
 })
 input.onButtonPressed(Button.AB, function () {
-    turtle.turnLeft()
+    turtle.back(1)
 })
 input.onButtonPressed(Button.B, function () {
     turtle.turnRight()
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    turtle.forward(1)
+})
+basic.showString("its MR.TURTLE")
